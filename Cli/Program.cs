ede8f03e8b6d2f9ac056caf9b635773dc084c40e
Pavel Perestoronin <eigenein@gmail.com>
2014-08-01 22:05:15 +0300

    Create solution

---
+++ Cli/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Cli
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
--- Cli/Properties/AssemblyInfo.cs
+++ Cli/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("wot-achievements")]
+[assembly: AssemblyTitle("Cli")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("wot-achievements")]
+[assembly: AssemblyProduct("Cli")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("915ca93a-35ee-4400-a16e-5fcbfd1c94e4")]
+[assembly: Guid("44af64c4-ec13-493b-8cba-ce274de93545")]
 
 // Version information for an assembly consists of the following four values:
 //
